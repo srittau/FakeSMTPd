9fffadd1d99b53ff3a98d8594c6a6d865d69cd47
Sebastian Rittau <srittau@rittau.biz>
2017-06-19 21:19:46 +0200

    do not configure logging for tests

--- fakesmtpd/server.py
+++ fakesmtpd/server.py
@@ -7,10 +7,9 @@ from fakesmtpd.connection import handle_connection
 SMTP_PORT = 25
 
 
-logging.basicConfig(level=logging.INFO)
-
-
 def main() -> None:
+    logging.basicConfig(level=logging.INFO)
+
     loop = asyncio.get_event_loop()
     loop.add_signal_handler(signal.SIGINT, loop.stop)
     loop.add_signal_handler(signal.SIGTERM, loop.stop)
