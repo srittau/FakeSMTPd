dd906386dfe243ccc74d999d94c244d93a482497
Sebastian Rittau <srittau@rittau.biz>
2017-06-19 21:31:12 +0200

    add -p option to choose the port

--- README.md
+++ README.md
@@ -4,3 +4,12 @@
 
 FakeSMTPd is an SMTP server for testing mail functionality. Any mail sent via
 this server will be printed to stdout, but will not be forwarded any further.
+
+Usage
+-----
+
+`fakesmtpd [OPTIONS]`
+
+Supported options:
+
+  * `-p, --port [PORT]` SMTP port to listen on
--- fakesmtpd/server.py
+++ fakesmtpd/server.py
@@ -1,6 +1,9 @@
+import argparse
 import asyncio
 import logging
 import signal
+import sys
+from typing import List
 
 from fakesmtpd.connection import handle_connection
 
@@ -9,10 +12,28 @@ SMTP_PORT = 25
 
 def main() -> None:
     logging.basicConfig(level=logging.INFO)
+    port = parse_args(sys.argv)
+    try:
+        run_server(port)
+    except PermissionError as exc:
+        print(str(exc), file=sys.stderr)
+        sys.exit(1)
 
+
+def parse_args(argv: List[str]) -> int:
+    parser = argparse.ArgumentParser(
+        description="SMTP server for testing mail functionality")
+    parser.add_argument(
+        "--port", "-p", type=int, nargs="?", default=SMTP_PORT,
+        help="SMTP port to listen on")
+    args = parser.parse_args()
+    return args.port
+
+
+def run_server(port: int) -> None:
     loop = asyncio.get_event_loop()
     loop.add_signal_handler(signal.SIGINT, loop.stop)
     loop.add_signal_handler(signal.SIGTERM, loop.stop)
-    s = asyncio.start_server(handle_connection, port=SMTP_PORT)
+    s = asyncio.start_server(handle_connection, port=port)
     loop.run_until_complete(s)
     loop.run_forever()
